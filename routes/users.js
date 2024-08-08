3182fdfe83803c57237593cbd526bfc423d5f502
Koharu0212 <k.o.19970212@gmail.com>
2024-08-08 13:33:58 +0900

    ユーザ登録

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,3 @@
 const router = require("express").Router(); //ルーティング設定
-const User = require("../models/User");
-
-//ユーザ登録
-router.post("/register", async (req, res) => {
-	try {
-		const newUser = await new User({
-			username: req.body.username,
-			email: req.body.email,
-			password: req.body.password,
-		});
-		const user = await newUser.save();
-		return res.status(200).json(user);
-	} catch (err) {
-		return res.status(500).json(err); //サーバ関連のエラー(このプログラムが間違っていた時)
-	}
-})
 
 module.exports = router;
