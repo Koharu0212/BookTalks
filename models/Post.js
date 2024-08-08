ec8915685753ff1aac3bbff0571cfdbc4e93aaae
Koharu0212 <k.o.19970212@gmail.com>
2024-08-08 15:18:00 +0900

    投稿用のスキーマを作成

---
+++ models/Post.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema(
+	{
+		userid: {
+			type: String,
+			required: true,
+		},
+		desc: { //投稿内容
+			type: String,
+			max: 500,
+		},
+		img: {
+			type: String,
+		},
+		likes: {
+			type: Array,
+			default: [],
+		},
+	},
+	{ timestamps: true },
+);
+
+module.exports = mongoose.model("Post", PostSchema);
