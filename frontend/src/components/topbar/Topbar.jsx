561cb5d9f43cc8f221f1bb40c117e90dbfbbfea1
Koharu0212 <k.o.19970212@gmail.com>
2024-08-09 12:35:31 +0900

    画像とTopbarを追加

---
+++ frontend/src/components/topbar/Topbar.jsx
@@ -0,0 +1,32 @@
+import React from "react"
+import { Search, Chat, Notifications } from "@mui/icons-material"
+import "./Topbar.css"
+
+export default function Topbar() {
+  return (
+	<div className="topbarContainer">
+		<div className="topbarLeft">
+			<span className="logo">Book Talks</span>
+		</div>
+		<div className="topbarCentre">
+			<div className="searchbar">
+				<Search className="searchIcon" />
+				<input type="text" className="searchInput" placeholder="探し物はなんですか？" />
+			</div>
+		</div>
+		<div className="topbarRight">
+			<div className="topbarItemIcons">
+				<div className="topbarIconItem">
+					<Chat />
+					<span className="topbarIconBadge">1</span>
+				</div>
+				<div className="topbarIconItem">
+					<Notifications />
+					<span className="topbarIconBadge">2</span>
+				</div>
+				<img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+			</div>
+		</div>
+	</div>
+  );
+}
