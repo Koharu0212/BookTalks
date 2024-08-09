838eadb29ea7a818fb3344ee7455a147f189009d
Koharu0212 <k.o.19970212@gmail.com>
2024-08-09 18:19:15 +0900

    ホーム画面右側のコンポーネントを追加

--- frontend/src/components/rightbar/Rightbar.jsx
+++ frontend/src/components/rightbar/Rightbar.jsx
@@ -3,6 +3,26 @@ import "./Rightbar.css"
 
 export default function Rightbar() {
   return (
-	<div className="rightbar">Rightbar</div>
+	<div className="rightbar">
+    <div className="rightbarWrapper">
+      <h4 className="rightbarTitle">オンラインの友達</h4>
+      <ul className="rightbarFriendList">
+        <li className="rightbarFriend">
+          <div className="rightbarProfileImgContainer">
+            <img src="assets/person/1.jpeg" alt="" className="rightbarProfileImg"/>
+            <span className="rightbarOnline"></span>
+          </div>
+          <span className="rightbarUserName">Shin Code</span>
+        </li>
+        <li className="rightbarFriend">
+          <div className="rightbarProfileImgContainer">
+            <img src="assets/person/2.jpeg" alt=""  className="rightbarProfileImg"/>
+            <span className="rightbarOnline"></span>
+          </div>
+          <span className="rightbarUserName">Tanaka</span>
+        </li>
+      </ul>
+    </div>
+  </div>
   )
 }
